c3c2e5cafae097cb18f6a3d64d19c4361b5bedec
Bjørn Hoxmark <bhoxmark@gmail.com>
2017-07-31 22:41:52 +0200

    first commit

---
+++ afterCleanInstall.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+
+# homebrew apps
+brew install brew-cask
+brew install git
+brew install git-extras
+brew install wget
+
+
+# cask apps
+brew cask install sublime-text3
+brew cask install vlc
+brew cask install dropbox
+brew cask install cakebrew
+brew cask install slack
+brew cask install appcleaner
+brew cask install firefox
+brew cask install skype
+brew cask install iterm2
+brew cask install alfred
+brew cask install google-chrome
+brew cask install cyberduck
+brew cask install dash
+brew cask install the-unarchiver
+brew cask install paparazzi
+brew cask install bartender
+brew cask install caffeine
+brew cask install cleanmymac
+brew cask install imageoptim
+brew cask install divvy
+brew cask install visual-studio-code
+
